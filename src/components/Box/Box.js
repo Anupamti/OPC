cea46d8d03abc875caf221e583d17bb461ca23b4
anupam.tiwari.namasys <anupam.tiwari@namasyslimited.onmicrosoft.com>
2021-09-11 16:54:43 +0530

    assignment 2

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Box.css'
-function Box({ props }) {
+function Box(props) {
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [age, setAge] = useState('')
+    const [hobbies, setHobbies] = useState('')
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        props.onFormSubmit({
+            name,
+            email,
+            age,
+            hobbies,
+
+        })
+        setName('');
+        setAge('');
+        setEmail('');
+        setHobbies('');
+    }
+
     return (
         <div className={"boxmain"}>
-            this is a box component with {props} props
+
+            <form onSubmit={handleFormSubmit} className={"form"}>
+                <div className="formGroup">
+                    <label className={"label"}> Name</label>
+                    <input value={name} onChange={(e) => setName(e.target.value)} className="input" />
+                </div>
+                <div className="formGroup">
+                    <label className={"label"}> Email</label>
+                    <input value={email} onChange={(e) => setEmail(e.target.value)} className="input" />
+                </div>
+                <div className="formGroup">
+                    <label className={"label"}> Age</label>
+                    <input value={age} onChange={(e) => setAge(e.target.value)} className="input" />
+                </div>
+                <div className="formGroup">
+                    <label className={"label"}> Hobbies</label>
+                    <input value={hobbies} onChange={(e) => setHobbies(e.target.value)} className="input" />
+                </div>
+                <input className={"inputButton"} type="submit" />
+            </form>
         </div>
     )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Box from '../Box/Box'
 import './Home.css'
 function Home() {
+
+    const [formData, setFormData] = useState()
+
+    const handleFormSubmit = (e) => {
+        setFormData(e)
+        console.log(e)
+    }
+
     return (
         <div className={"main"}>
-            <h3> this is a home component and Im sending diffrent props to the box component </h3>
+            <h3>Sate Up lifting </h3>
+            {
+                formData && (
+                    <>
+                        getting the data from the child to parent component
+                        <div style={{ display: "flex", justifyContent: 'space-evenly', width: '80%' }}>
+
+                            <p>{formData?.name}</p>
+                            <p>{formData?.age}</p>
+                            <p>{formData?.email}</p>
+                            <p>{formData?.hobbies}</p>
+                        </div>
+                    </>
+                )
+            }
             <div className={"sub"}>
-                <Box props="One" />
-                <Box props="two" />
-                <Box props="three" />
-                <Box props="four" />
+                <Box onFormSubmit={handleFormSubmit} />
             </div>
 
         </div>
